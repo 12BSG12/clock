84583b54ca9c7e91627ada799c9d0b0ae6c7f6fc
12BSG12 <carlj8335@gmail.com>
2022-11-28 22:57:36 +0500

    fix

--- src/Labs/Lab6.jsx
+++ src/Labs/Lab6.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import styles from '../App.module.css';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { FormGroup, InputLabel, Slider } from '@mui/material';
 import {
   PresentationControls,
   Environment,
@@ -14,18 +10,12 @@ import {
   OrbitControls,
 } from '@react-three/drei';
 
-const Pentagon = ({ position, rotate, speed, args, radius, time }) => {
-  const ref = React.useRef('null');
-  useFrame(() => (ref.current.rotation[rotate] += speed));
-
+const Clock = ({ position, args, radius, time }) => {
   return (
-    <mesh ref={ref} position={position}>
+    <mesh position={position}>
       <RoundedBox args={args} radius={radius}>
         <meshPhongMaterial color="#03CEA4" />
-        <Html
-          distanceFactor={18}
-          position={[0, 0, 0.50]}
-          transform>
+        <Html distanceFactor={18} position={[0, 0, 0.5]} transform>
           <span>{time}</span>
         </Html>
       </RoundedBox>
@@ -33,42 +23,26 @@ const Pentagon = ({ position, rotate, speed, args, radius, time }) => {
   );
 };
 
-const SelectSmall = ({ rotate, setRotate, setSpeed, speed }) => {
+const ClockLegs = ({ position, args, radius, rotation }) => {
   return (
-    <FormGroup row sx={{ mb: 5 }}>
-      <FormControl sx={{ mt: 2, mb: 1, minWidth: 135, color: 'white' }} size="small">
-        <InputLabel sx={{ color: 'white' }} id="demo-select-small">
-          Вращать по
-        </InputLabel>
-        <Select
-          sx={{ color: 'white' }}
-          value={rotate}
-          label="Вращать по"
-          onChange={(e) => setRotate(e.target.value)}>
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value="x">X</MenuItem>
-          <MenuItem value="y">Y</MenuItem>
-          <MenuItem value="z">Z</MenuItem>
-        </Select>
-      </FormControl>
-      <Slider
-        step={0.01}
-        valueLabelDisplay="auto"
-        min={0.01}
-        max={100}
-        value={speed}
-        onChange={(e) => setSpeed(e.target.value)}
-      />
-    </FormGroup>
+    <mesh position={position}>
+      <RoundedBox args={args} radius={radius} rotation={rotation}>
+        <meshPhongMaterial color="#18181f" />
+      </RoundedBox>
+    </mesh>
+  );
+};
+const ClockBtn= ({ position, args, radius, rotation }) => {
+  return (
+    <mesh position={position}>
+      <RoundedBox args={args} radius={radius} rotation={rotation}>
+        <meshPhongMaterial color="#18181f" />
+      </RoundedBox>
+    </mesh>
   );
 };
 
-const Lab6 = () => {
-  const [rotate, setRotate] = React.useState('');
-  const [speed, setSpeed] = React.useState(0.01);
-
+const Lab7 = () => {
   const [time, setTime] = React.useState(new Date().toLocaleTimeString());
 
   React.useEffect(() => {
@@ -81,9 +55,7 @@ const Lab6 = () => {
   return (
     <div className={styles.root}>
       <p>Лабораторная работа №7 </p>
-      <SelectSmall rotate={rotate} setRotate={setRotate} speed={speed} setSpeed={setSpeed} />
-      <Canvas 
-         shadows dpr={[1, 2]} camera={{ position: [4, 1, 4], fov: 50 }}>
+      <Canvas shadows dpr={[1, 2]} camera={{ position: [4, 1, 4], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <spotLight
           position={[10, 10, 10]}
@@ -99,22 +71,61 @@ const Lab6 = () => {
           rotation={[0, 0.8, 0]}
           polar={[-Math.PI / 3, Math.PI / 3]}
           azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-          <Pentagon
+          <Clock color="#03CEA4" args={[3, 1, 1]} position={[0, 1, -2]} radius={0.05} time={time} />
+          <ClockBtn
             color="#03CEA4"
-            args={[3, 1, 1]}
-            position={[0, 1, -2]}
+            args={[0.3, 0.2, 0.2]}
+            position={[1.1, 1.5, -1.7]}
             radius={0.05}
-            rotate={rotate}
-            speed={speed}
-            time={time}
+          />
+          <ClockBtn
+            color="#03CEA4"
+            args={[0.3, 0.2, 0.2]}
+            position={[0.6, 1.5, -1.7]}
+            radius={0.05}
+          />
+          <ClockBtn
+            color="#03CEA4"
+            args={[0.3, 0.2, 0.2]}
+            position={[0.1, 1.5, -1.7]}
+            radius={0.05}
+          />
+
+          <ClockLegs
+            color="#03CEA4"
+            args={[1.8, 0.2, 0.2]}
+            position={[1.2, 0, -2]}
+            radius={0.05}
+            rotation={[0.2, 1.6, 0.5]}
+          />
+          <ClockLegs
+            color="#03CEA4"
+            args={[1.8, 0.2, 0.2]}
+            position={[1.2, 0, -2]}
+            radius={0.05}
+            rotation={[0.2, 1.6, -0.9]}
+          />
+          <ClockLegs
+            color="#03CEA4"
+            args={[1.8, 0.2, 0.2]}
+            position={[-1.2, 0, -2]}
+            radius={0.05}
+            rotation={[0.2, 1.6, 0.5]}
+          />
+          <ClockLegs
+            color="#03CEA4"
+            args={[1.8, 0.2, 0.2]}
+            position={[-1.2, 0, -2]}
+            radius={0.05}
+            rotation={[0.2, 1.6, -0.9]}
           />
         </PresentationControls>
         <ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />
         <Environment preset="city" />
-        <OrbitControls/>
+        <OrbitControls />
       </Canvas>
     </div>
   );
 };
 
-export default Lab6;
+export default Lab7;
